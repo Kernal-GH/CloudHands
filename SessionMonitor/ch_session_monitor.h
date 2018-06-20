2861f969c17ae021981a17ae795607e49c8457ad
shajf <csp001314@gmail.com>
2018-06-20 19:45:00 +0800

    add session monitor

---
+++ SessionMonitor/ch_session_monitor.h
@@ -0,0 +1,30 @@
+/*
+ *
+ *      Filename: ch_session_monitor.h
+ *
+ *        Author: shajf,csp001314@gmail.com
+ *   Description: ---
+ *        Create: 2017-11-09 17:40:14
+ * Last Modified: 2017-11-09 17:40:14
+ */
+
+#ifndef CH_SESSION_MONITOR_H
+#define CH_SESSION_MONITOR_H
+
+typedef struct ch_session_monitor_t ch_session_monitor_t;
+typedef struct ch_session_monitor_item_t ch_session_monitor_item_t;
+
+
+struct ch_session_monitor_t {
+
+
+};
+
+
+struct ch_session_monitor_item_t {
+
+
+};
+
+
+#endif /*CH_SESSION_MONITOR_H*/
