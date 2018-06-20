ab5493778d293d43ea5e7294116d6cc667460062
shajf <csp001314@gmail.com>
2018-06-20 15:27:49 +0800

    add dpdk 17.08.2

--- build/dpdk.sh
+++ build/dpdk.sh
@@ -5,9 +5,10 @@
 #        Author: shajf,csp001314@gmail.com
 #   Description: ---
 #        Create: 2018-06-19 17:56:46
-# Last Modified: 2018-06-19 18:15:49
+# Last Modified: 2018-06-20 15:26:15
 #
 
 dpdk_ver=17.05.2
+#dpdk_ver=17.08.2
 #dpdk_ver=18.02.1
 
