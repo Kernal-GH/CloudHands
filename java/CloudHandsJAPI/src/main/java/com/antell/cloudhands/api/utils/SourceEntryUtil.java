40c16cfa26c03c633bf5e576c7818196fd7fa034
shajf <csp001314@gmail.com>
2018-06-27 16:29:55 +0800

    SourceEntry mapto Implemented Class

---
+++ java/CloudHandsJAPI/src/main/java/com/antell/cloudhands/api/utils/SourceEntryUtil.java
@@ -0,0 +1,101 @@
+package com.antell.cloudhands.api.utils;
+
+import com.antell.cloudhands.api.packet.ARPPacket;
+import com.antell.cloudhands.api.packet.ICMPPacket;
+import com.antell.cloudhands.api.packet.TCPSession;
+import com.antell.cloudhands.api.packet.UDPSession;
+import com.antell.cloudhands.api.packet.tcp.http.HTTPSession;
+import com.antell.cloudhands.api.packet.tcp.mail.MailSession;
+import com.antell.cloudhands.api.packet.udp.dns.DNSSession;
+import com.antell.cloudhands.api.source.SourceEntry;
+
+/**
+ * Created by dell on 2018/6/27.
+ */
+public class SourceEntryUtil {
+
+    private SourceEntryUtil(){}
+
+    public static boolean isHttpSession(SourceEntry sourceEntry){
+
+        return sourceEntry instanceof HTTPSession;
+    }
+
+    public static HTTPSession toHttpSession(SourceEntry sourceEntry){
+
+        return (HTTPSession)sourceEntry;
+    }
+
+    public static boolean isHttpSessionWithAttack(SourceEntry sourceEntry){
+
+        HTTPSession httpSession = toHttpSession(sourceEntry);
+
+        return httpSession.getSecMatchResult()!=null;
+    }
+
+    public static boolean isMailSession(SourceEntry sourceEntry){
+
+        return sourceEntry instanceof MailSession;
+    }
+
+    public static MailSession toMailSession(SourceEntry sourceEntry){
+
+        return (MailSession)sourceEntry;
+    }
+
+    public static boolean isMailSessionWithAttack(SourceEntry sourceEntry){
+
+        MailSession mailSession = toMailSession(sourceEntry);
+        return mailSession.getSecMatchResult()!=null;
+    }
+
+    public static boolean isTCPSession(SourceEntry sourceEntry){
+
+        return sourceEntry instanceof TCPSession;
+    }
+
+    public static TCPSession toTCPSession(SourceEntry sourceEntry){
+
+        return (TCPSession)sourceEntry;
+    }
+
+    public static boolean isUDPSession(SourceEntry sourceEntry){
+
+        return sourceEntry instanceof UDPSession;
+    }
+
+    public static UDPSession toUDPSession(SourceEntry sourceEntry){
+
+        return (UDPSession)sourceEntry;
+    }
+
+    public static boolean isDNSSession(SourceEntry sourceEntry){
+
+        return sourceEntry instanceof DNSSession;
+    }
+
+    public static DNSSession toDNSSession(SourceEntry sourceEntry){
+
+        return (DNSSession)sourceEntry;
+    }
+
+    public static boolean isArpPacket(SourceEntry sourceEntry){
+
+        return sourceEntry instanceof ARPPacket;
+    }
+
+    public static ARPPacket toARPPacket(SourceEntry sourceEntry){
+
+        return (ARPPacket) sourceEntry;
+    }
+
+    public static boolean isICMPPacket(SourceEntry sourceEntry){
+
+        return sourceEntry instanceof ICMPPacket;
+    }
+
+    public static ICMPPacket toICMPPacket(SourceEntry sourceEntry){
+
+        return (ICMPPacket) sourceEntry;
+    }
+}
