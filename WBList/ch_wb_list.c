0ff95f870de155aa2fa2513eccdda082fa851a62
shajf <csp001314@gmail.com>
2018-09-05 15:05:29 +0800

    add ip&port white&black list match

--- WBList/ch_wb_list.c
+++ WBList/ch_wb_list.c
@@ -5,40 +5,36 @@
  *        Author: shajf,csp001314@gmail.com
  *   Description: ---
  *        Create: 2018-09-03 18:16:04
- * Last Modified: 2018-09-03 19:40:46
+ * Last Modified: 2018-09-05 15:03:19
  */
 
 #include "ch_wb_list.h"
 
-int ch_wb_list_init(ch_wb_list_t *wb_list,const char *mmap_fname,size_t msize,
-	int is_on,
-	int is_black_list,
-	int (*load)(ch_wb_list_t *wb_list),
-	void (*unload)(ch_wb_list_t *wb_list),
-	int (*add_range_num)(ch_wb_list_t *wb_list,uint64_t start,uint64_t end),
-	int (*add_str)(ch_wb_list_t *wb_list,const char *v),
-	void (*del)(ch_wb_list_t *wb_list,uint64_t id),
-	int (*is_match_str)(ch_wb_list_t *wb_list,const char *key),
-	int (*is_match_num)(ch_wb_list_t *wb_list,uint64_t v),
-	ch_wb_list_iter_t *(*wb_list_iter_prepare)(ch_wb_list_t *wb_list)) {
-
+int ch_wb_list_init(ch_wb_list_t *wb_list,const char *mmap_fname,size_t msize,uint32_t entry_size,
+	int (*add)(ch_wb_list_t *wb_list,ch_wb_list_entry_t *entry,void *value),
+	void (*entry_copy)(ch_wb_list_t *wb_list,ch_wb_list_entry_t *d_entry,ch_wb_list_entry_t *s_entry),
+	int (*is_match)(ch_wb_list_t *wb_list,ch_wb_list_entry_t *entry,void *match_value),
+	void (*dump)(ch_wb_list_t *wb_list,ch_wb_list_entry_t *entry,FILE *out)){
 
 	if(ch_wb_memory_init(&wb_list->wbm,mmap_fname,msize))
 		return -1;
 
-	wb_list->is_on = is_on;
-	wb_list->is_black_list = is_black_list;
-	wb_list->load = load;
-	wb_list->unload = unload;
-	wb_list->add_range_num = add_range_num;
-	wb_list->add_str = add_str;
-	wb_list->del = del;
-	wb_list->is_match_str = is_match_str;
-	wb_list->is_match_num = is_match_num;
-	wb_list->wb_list_iter_prepare = wb_list_iter_prepare;
+	wb_list->add = add;
+	wb_list->entry_copy = entry_copy;
+	wb_list->is_match = is_match;
+	wb_list->dump = dump;
+	
+	wb_list->header = (ch_wb_list_header_t*)ch_wb_memory_alloc(&wb_list->wbm,sizeof(ch_wb_list_header_t));
+	if(wb_list->header == NULL)
+		return -1;
 
-	if(wb_list->load)
-		return wb_list->load(wb_list);
+
+	wb_list->header->entries_num = (wb_list->wbm.end-wb_list->wbm.pos)/entry_size;
+	wb_list->header->free_entry_pos = 0;
+	wb_list->header->entry_size = entry_size;
+	wb_list->header->next_id = 12345;
+	wb_list->entries =(void*)(wb_list->header+1);
+
 
 	return 0;
 }
@@ -46,56 +42,110 @@ int ch_wb_list_init(ch_wb_list_t *wb_list,const char *mmap_fname,size_t msize,
 
 void ch_wb_list_fin(ch_wb_list_t *wb_list) {
 
-	if(wb_list->unload)
-		wb_list->unload(wb_list);
-
 	ch_wb_memory_fin(&wb_list->wbm);
 
 
 }
 
-int ch_wb_list_add_range_num(ch_wb_list_t *wb_list,uint64_t start,uint64_t end){
+int ch_wb_list_add(ch_wb_list_t *wb_list,void *value){
 
-	if(wb_list->add_range_num == NULL)
+	int rc;
+
+	ch_wb_list_entry_t *entry = ch_wb_list_entry_get(wb_list,wb_list->header->free_entry_pos);
+	if(entry == NULL)
 		return -1;
 
-	return wb_list->add_range_num(wb_list,start,end);
+	entry->id = ch_wb_list_alloc_id(wb_list);
+
+	rc = wb_list->add(wb_list,entry,value);
+
+	if(rc){
+	
+		/*error*/
+		wb_list->header->free_entry_pos -=1;
+	}else{
+	
+		wb_list->header->free_entry_pos +=1;	
+	}
+
+	return rc;
+}
+
+
+static void entries_move(ch_wb_list_t *wb_list,ch_wb_list_entry_t *start_entry){
+
+	ch_wb_list_entry_t *cur_entry = start_entry;
+	ch_wb_list_entry_t *next_entry = (ch_wb_list_entry_t*)ch_wb_list_entry_next(wb_list,(void*)start_entry);
+
+	while(next_entry){
+
+		cur_entry->id = next_entry->id;
+		ITEM_COPY(cur_entry,next_entry);
+		cur_entry = next_entry;
+		next_entry = (ch_wb_list_entry_t*)ch_wb_list_entry_next(wb_list,(void*)cur_entry);   
+	}
+
 
 }
 
-int ch_wb_list_add_str(ch_wb_list_t *wb_list,const char *v){
+static ch_wb_list_entry_t *_find_entry(ch_wb_list_t *wb_list,uint64_t id){
 
-	if(wb_list->add_str == NULL)
-		return -1;
-	return wb_list->add_str(wb_list,v);
-}
+	ch_wb_list_entry_t *entry;
 
-void ch_wb_list_del(ch_wb_list_t *wb_list,uint64_t id){
-
-	if(wb_list->del)
-		wb_list->del(wb_list,id);
-}
-
-int ch_wb_list_is_match_str(ch_wb_list_t *wb_list,const char *key){
-
-	if(wb_list->is_match_str)
-		return wb_list->is_match_str(wb_list,key);
-
-	return 0;
-}
-
-int ch_wb_list_is_match_num(ch_wb_list_t *wb_list,uint64_t v){
-
-	if(wb_list->is_match_num)
-		return wb_list->is_match_num(wb_list,v);
-	return 0;
-}
-
-ch_wb_list_iter_t * ch_wb_list_iter_prepare(ch_wb_list_t *wb_list){
-
-	if(wb_list->wb_list_iter_prepare)
-		return wb_list->wb_list_iter_prepare(wb_list);
+	ch_wb_list_for_each_entry(wb_list,entry,ch_wb_list_entry_t){
+	
+		if(entry->id == id)
+			return entry;
+	}
 
 	return NULL;
 }
 
+void ch_wb_list_del(ch_wb_list_t *wb_list,uint64_t id){
+
+	ch_wb_list_entry_t *entry;
+
+	if(ch_wb_list_empty(wb_list))
+		return;
+
+	entry = _find_entry(wb_list,id);
+	if(entry == NULL)
+		return;
+
+	entries_move(wb_list,entry);
+	wb_list->header->free_entry_pos-=1;
+
+}
+
+int ch_wb_list_is_match(ch_wb_list_t *wb_list,void *match_value){
+
+	ch_wb_list_entry_t *entry;
+
+	if(ch_wb_list_empty(wb_list))
+		return 0;
+
+	ch_wb_list_for_each_entry(wb_list,entry,ch_wb_list_entry_t){
+	
+		if(wb_list->is_match(wb_list,entry,match_value))
+			return 1;
+	}
+
+	return 0;
+}
+
+void ch_wb_list_dump(ch_wb_list_t *wb_list,FILE *out){
+
+	ch_wb_list_entry_t *entry;
+	fprintf(out,"Dump The White&Black List Informations:\n");
+	fprintf(out,"Entries Number:%lu\n",(unsigned long)wb_list->header->entries_num);
+	fprintf(out,"Current Free Entry Pos:%lu\n",(unsigned long)wb_list->header->free_entry_pos);
+
+	fprintf(out,"Dump The Entries Informations:\n");
+	ch_wb_list_for_each_entry(wb_list,entry,ch_wb_list_entry_t){
+	
+		wb_list->dump(wb_list,entry,out);
+
+	}
+
+
+}
--- WBList/ch_wb_list.h
+++ WBList/ch_wb_list.h
@@ -11,66 +11,101 @@
 #ifndef CH_WB_LIST_H
 #define CH_WB_LIST_H
  
+typedef struct ch_wb_list_header_t ch_wb_list_header_t;
 typedef struct ch_wb_list_t ch_wb_list_t;
-typedef struct ch_wb_list_iter_t ch_wb_list_iter_t;
+typedef struct ch_wb_list_entry_t ch_wb_list_entry_t;
 
 #include "ch_wb_memory.h"
 
+#pragma pack(push,1)
+struct ch_wb_list_header_t {
 
-struct ch_wb_list_iter_t {
+	uint32_t entries_num;
+	uint32_t free_entry_pos;
 
-	ch_wb_list_t *wb_list;
+	uint32_t entry_size;
+	uint64_t next_id;
 
-	void *(*next)(ch_wb_list_iter_t *iter);
+};
+#pragma pack(pop)
+
+struct ch_wb_list_entry_t {
+
+	uint64_t id;
 
 };
 
 struct ch_wb_list_t {
 
 	ch_wb_memory_t wbm;
-
-	int is_on;
-	int is_black_list;
-
-	int (*load)(ch_wb_list_t *wb_list);
-	void (*unload)(ch_wb_list_t *wb_list);
-
-	int (*add_range_num)(ch_wb_list_t *wb_list,uint64_t start,uint64_t end);
-	int (*add_str)(ch_wb_list_t *wb_list,const char *v);
+	ch_wb_list_header_t *header;
 	
-	void (*del)(ch_wb_list_t *wb_list,uint64_t id);
+	void *entries;
 
-	int (*is_match_str)(ch_wb_list_t *wb_list,const char *key);
-	int (*is_match_num)(ch_wb_list_t *wb_list,uint64_t v);
+	int (*add)(ch_wb_list_t *wb_list,ch_wb_list_entry_t *entry,void *value);
+	
+	void (*entry_copy)(ch_wb_list_t *wb_list,ch_wb_list_entry_t *d_entry,ch_wb_list_entry_t *s_entry);
 
-	ch_wb_list_iter_t *(*wb_list_iter_prepare)(ch_wb_list_t *wb_list);
+	int  (*is_match)(ch_wb_list_t *wb_list,ch_wb_list_entry_t *entry,void *match_value);
+	void (*dump)(ch_wb_list_t *wb_list,ch_wb_list_entry_t *entry,FILE *out);
 
 };
 
-extern int ch_wb_list_init(ch_wb_list_t *wb_list,const char *mmap_fname,size_t msize,
-	int is_on,
-	int is_black_list,
-	int (*load)(ch_wb_list_t *wb_list),
-	void (*unload)(ch_wb_list_t *wb_list),
-	int (*add_range_num)(ch_wb_list_t *wb_list,uint64_t start,uint64_t end),
-	int (*add_str)(ch_wb_list_t *wb_list,const char *v),
-	void (*del)(ch_wb_list_t *wb_list,uint64_t id),
-	int (*is_match_str)(ch_wb_list_t *wb_list,const char *key),
-	int (*is_match_num)(ch_wb_list_t *wb_list,uint64_t v),
-	ch_wb_list_iter_t *(*wb_list_iter_prepare)(ch_wb_list_t *wb_list));
+static inline uint64_t ch_wb_list_alloc_id(ch_wb_list_t *wb_list){
+
+	uint64_t id = wb_list->header->next_id;
+
+	wb_list->header->next_id = (wb_list->header->next_id+1)%UINT64_MAX;
+
+	 return id;
+}
+
+static inline void *ch_wb_list_entry_get(ch_wb_list_t *wb_list,uint32_t indx){
+
+	void *entry = wb_list->entries+indx*wb_list->header->entry_size;
+	if(entry>=wb_list->wbm.end)
+		return NULL;
+
+	return entry;
+}
+
+static inline void *ch_wb_list_entry_first(ch_wb_list_t *wb_list){
+
+	if(wb_list->header->free_entry_pos == 0)
+		return NULL;
+
+	return wb_list->entries;
+}
+
+static inline void *ch_wb_list_entry_next(ch_wb_list_t *wb_list,void *entry){
+
+	void *free_entry = ch_wb_list_entry_get(wb_list,wb_list->header->free_entry_pos);
+	void *next_entry = entry+wb_list->header->entry_size;
+
+	return next_entry>=free_entry?NULL:next_entry;
+}
+
+#define ch_wb_list_empty(wb_list) (wb_list->header->free_entry_pos == 0)
+
+#define ch_wb_list_for_each_entry(wb_list,entry,type) \
+	for(entry = (type*)ch_wb_list_entry_first(wb_list); \
+		entry!=NULL; \
+		entry = (type*)ch_wb_list_entry_next(wb_list,(void*)entry))
+
+extern int ch_wb_list_init(ch_wb_list_t *wb_list,const char *mmap_fname,size_t msize,uint32_t entry_size,
+	int (*add)(ch_wb_list_t *wb_list,ch_wb_list_entry_t *entry,void *value),
+	void (*entry_copy)(ch_wb_list_t *wb_list,ch_wb_list_entry_t *d_entry,ch_wb_list_entry_t *s_entry),
+	int (*is_match)(ch_wb_list_t *wb_list,ch_wb_list_entry_t *entry,void *match_value),
+	void (*dump)(ch_wb_list_t *wb_list,ch_wb_list_entry_t *entry,FILE *out));
 
 extern void ch_wb_list_fin(ch_wb_list_t *wb_list);
 
-extern int ch_wb_list_add_range_num(ch_wb_list_t *wb_list,uint64_t start,uint64_t end);
-
-extern int ch_wb_list_add_str(ch_wb_list_t *wb_list,const char *v);
+extern int ch_wb_list_add(ch_wb_list_t *wb_list,void *value);
 
 extern void ch_wb_list_del(ch_wb_list_t *wb_list,uint64_t id);
 
-extern int ch_wb_list_is_match_str(ch_wb_list_t *wb_list,const char *key);
+extern int ch_wb_list_is_match(ch_wb_list_t *wb_list,void *match_value);
 
-extern int ch_wb_list_is_match_num(ch_wb_list_t *wb_list,uint64_t v);
-
-extern ch_wb_list_iter_t * ch_wb_list_iter_prepare(ch_wb_list_t *wb_list);
+extern void ch_wb_list_dump(ch_wb_list_t *wb_list,FILE *out);
 
 #endif /*CH_WB_LIST_H*/
