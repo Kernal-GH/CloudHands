e30e1637db31dbcbd83f34f36a88bacdba7f71f6
shajf <csp001314@gmail.com>
2018-10-31 12:10:42 +0800

    add mysql codes

---
+++ tcp/app/mysql/ch_mysql_request.h
@@ -0,0 +1,21 @@
+/*
+ *
+ *      Filename: ch_mysql_request.h
+ *
+ *        Author: shajf,csp001314@gmail.com
+ *   Description: ---
+ *        Create: 2018-10-31 12:03:36
+ * Last Modified: 2018-10-31 12:03:36
+ */
+
+#ifndef CH_MYSQL_REQUEST_H
+#define CH_MYSQL_REQUEST_H
+
+typedef struct ch_mysql_request_t ch_mysql_request_t;
+
+struct ch_mysql_request_t {
+
+
+};
+
+#endif /*CH_MYSQL_REQUEST_H*/
---
+++ tcp/app/mysql/ch_mysql_response.h
@@ -0,0 +1,21 @@
+/*
+ *
+ *      Filename: ch_mysql_response.h
+ *
+ *        Author: shajf,csp001314@gmail.com
+ *   Description: ---
+ *        Create: 2018-10-31 12:05:05
+ * Last Modified: 2018-10-31 12:05:05
+ */
+
+#ifndef CH_MYSQL_RESPONSE_H
+#define CH_MYSQL_RESPONSE_H
+
+typedef struct ch_mysql_response_t ch_mysql_response_t;
+
+struct ch_mysql_response_t {
+
+
+};
+
+#endif /*CH_MYSQL_RESPONSE_H*/
