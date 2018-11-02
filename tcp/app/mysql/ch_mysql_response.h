db1824484515c74e79bf9ef18e792f916b4e098b
shajf <csp001314@gmail.com>
2018-11-02 15:54:55 +0800

    code mysql field data struct

--- tcp/app/mysql/ch_mysql_response.h
+++ tcp/app/mysql/ch_mysql_response.h
@@ -12,6 +12,44 @@
 #define CH_MYSQL_RESPONSE_H
 
 typedef struct ch_mysql_response_t ch_mysql_response_t;
+typedef struct ch_mysql_field_pool_t ch_mysql_field_pool_t;
+typedef struct ch_mysql_field_t ch_mysql_field_t;
+typedef struct ch_mysql_row_t ch_mysql_row_t;
+
+
+struct ch_mysql_field_pool_t {
+
+	uint32_t field_num;
+	uint32_t extra_num;
+
+	struct list_head field_list;
+	struct list_head row_list;
+};
+
+struct ch_mysql_field_t {
+
+	struct list_head node;
+	
+	const char *catalog;
+	const char *db;
+	const char *table;
+	const char *org_table;
+	const char *name;
+	const char *org_name;
+	
+	uint16_t charset;
+	uint32_t length;
+	uint8_t  type;
+	uint16_t flags;
+	uint8_t decimals;
+	
+	const char *dft;
+};
+
+struct ch_mysql_row_t {
+	struct list_head node;
+	const char *value;
+};
 
 struct ch_mysql_response_t {
 
