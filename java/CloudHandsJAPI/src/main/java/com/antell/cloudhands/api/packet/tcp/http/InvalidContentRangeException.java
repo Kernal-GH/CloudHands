2506670b2121affe73adc6d92f331031ec07aa1e
shajf <csp001314@163.com>
2018-11-10 08:46:29 +0800

    del src

--- java/CloudHandsJAPI/src/main/java/com/antell/cloudhands/api/packet/tcp/http/InvalidContentRangeException.java
+++
@@ -1,9 +0,0 @@
-package com.antell.cloudhands.api.packet.tcp.http;
-
-public class InvalidContentRangeException extends Exception {
-
-    public InvalidContentRangeException(String e){
-
-        super(e);
-    }
-}
