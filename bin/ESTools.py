a3f46c020f038e890c42bd533038454cc6412f72
shajf <shajianfeng@antell.com.cn>
2019-04-03 18:15:21 +0800

    add new methods

--- bin/ESTools.py
+++ bin/ESTools.py
@@ -15,11 +15,50 @@ def del_es_index(es):
     for ind in indices:
         es.indices.delete(ind)
 
+def find_terms_topN(es):
+
+    if len(sys.argv)<8:
+        print("Usage:<host><cmd><index><doctype><field><topN><isDesc>")
+        sys.exit(-1)
+
+    index = sys.argv[3]
+    doc = sys.argv[4]
+    field = sys.argv[5]
+
+    topN = int(sys.argv[6])
+
+    orderS ="asc"
+    if sys.argv[7] == "true":
+        orderS = "desc"
+
+    query = {
+            "query": {
+                    "match_all": {}
+
+                },
+              "size":0,
+              "aggs": {
+                  "group_terms": {
+                      "terms": {"field": field,
+                       "order" : { "_count" : orderS},
+                        "size":topN
+                        }
+                      }
+                  }
+              }
+
+    results = es.search(index,doc,query)['aggregations']['group_terms']['buckets']
+
+    for item in results:
+        print(item['key'])
+
 
 def handle_cmd(es,cmd):
 
     if cmd == "del":
         del_es_index(es)
+    elif cmd == "term_topN":
+        find_terms_topN(es)
 
 
 if __name__ == '__main__':
